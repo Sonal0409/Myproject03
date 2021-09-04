16af17b764c153327e718e1316bf55ca58b93a79
Sonal <mittal.sonal04@gmail.com>
2021-09-04 22:57:40 +0530

    added new files

---
+++ src/test/java/com/myapp/qa/Tests/AddressbookDemo.java
@@ -0,0 +1,58 @@
+package com.myapp.qa.Tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AddressbookDemo {
+	
+	@Test
+	public void setup() throws InterruptedException
+	{
+		// Start chrome browser
+		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		// open the addressboo app on the chrome browser
+		
+		driver.get("http://18.222.140.171:49155/addressbook/");
+		
+		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+	
+	
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div/div/div/div[1]/div/div/div[2]/div")).click();
+		
+	Thread.sleep(2000)	;
+	
+	driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/table/tbody/tr[2]/td[3]/input")).sendKeys("Sonalnew123");
+		
+	Thread.sleep(2000)	;
+		
+	driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/table/tbody/tr[3]/td[3]/input")).sendKeys("Mittalnew123");
+		
+	Thread.sleep(2000);
+	// comment here 
+	driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/table/tbody/tr[5]/td[3]/input")).sendKeys("abc@gmail.com");
+	
+	Thread.sleep(2000);
+	driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/table/tbody/tr[6]/td[3]/div/input")).sendKeys("5/19/21,");
+	
+	Thread.sleep(3000);
+	
+	driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div/table/tbody/tr[1]/td[3]/div/div[1]/div")).click();
+	
+	Thread.sleep(3000);
+	
+	System.out.println(driver.getTitle());
+	
+ driver.close();	
+		
+	}
+
+}
