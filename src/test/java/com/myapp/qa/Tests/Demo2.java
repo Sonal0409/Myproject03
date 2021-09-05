e5f2a4a52b7ef6a541d9f9b5366d0c52886b9397
Sonal <mittal.sonal04@gmail.com>
2021-09-05 22:17:07 +0530

    demo2

--- src/test/java/com/myapp/qa/Tests/Demo2.java
+++ src/test/java/com/myapp/qa/Tests/Demo2.java
@@ -1,5 +1,30 @@
 package com.myapp.qa.Tests;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Demo2 {
+	
+	@Test
+	public void fetchCurrentURL() throws InterruptedException
+	{
+		// Start chrome browser
+		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		// open the addressboo app on the chrome browser
+		
+		driver.get("http://3.21.75.105:49153/addressbook/");
+		
+		driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+		Thread.sleep(2000);
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
 
 }
